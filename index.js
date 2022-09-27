4209a239be4b907611dbbc4c8419233546f00118
Moringa School <dwangui799@gmail.com>
2022-09-27 10:45:02 +0300

    completed  assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = () =>2000 / 100
+
+
+
+const add =(parameter1, parameter2) => parameter1 + parameter2;
+add (2,3); //=>
+
+
+const squre =(parameter4) => parameter4 * parameter4;
+
+
+
+
+
+
+
+
+
+
